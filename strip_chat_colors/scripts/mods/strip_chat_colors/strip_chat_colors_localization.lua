ed8ad005c037b694621c175cb17382c514aee290
Madison Marvuglia <89117064+MaddyMarv@users.noreply.github.com>
2026-08-17 22:44:33 -0400

    Update strip_chat_colors_localization.lua

--- strip_chat_colors/scripts/mods/strip_chat_colors/strip_chat_colors_localization.lua
+++ strip_chat_colors/scripts/mods/strip_chat_colors/strip_chat_colors_localization.lua
@@ -10,32 +10,17 @@ return {
 	},
 	strip_colors = {
 		en = "Strip Custom Colors",
-		description = {
-			en = "Removes {#color(...)} tags from chat messages.",
-		}
 	},
 	strip_fonts = {
 		en = "Strip Custom Fonts",
-		description = {
-			en = "Removes {#font(...)} tags from chat messages to force the default chat font.",
-		}
 	},
 	strip_sizes = {
 		en = "Strip Custom Sizes",
-		description = {
-			en = "Removes {#size(...)} tags from chat messages to force the default chat size.",
-		}
 	},
 	strip_decorations = {
 		en = "Strip Underline & Strikethrough",
-		description = {
-			en = "Removes {#under(...)} and {#strike(...)} tags from chat messages.",
-		}
 	},
 	force_lowercase = {
 		en = "Force All Lowercase",
-		description = {
-			en = "Forces all incoming chat messages to be entirely lowercase.",
-		}
 	},
 }
